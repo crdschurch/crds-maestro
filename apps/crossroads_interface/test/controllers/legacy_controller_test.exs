29c981f8ec7b1f7f7a61c0f05704b37604aed396
Michael Vezzani <mvezzani@callibrity.com>
2017-03-24 15:20:07 -0400

    feature/US7482 Mock calls in tests
    This commit mocks calls that were reaching outside of the Phoenix
    project

--- apps/crossroads_interface/test/controllers/legacy_controller_test.exs
+++ apps/crossroads_interface/test/controllers/legacy_controller_test.exs
@@ -1,9 +1,31 @@
 defmodule CrossroadsInterface.LegacyControllerTest do
   use CrossroadsInterface.ConnCase
+  alias CrossroadsContent.Pages
+  import Mock
+
+  @content_block_call %{"contentBlocks" => [%{"id" => 1, "title" => "generalError"}]}
+
+  @system_page_response %{"systemPages" => [%{"bodyClasses" => nil,
+                                              "card" => "summary",
+                                              "className" => "SystemPage",
+                                              "created" => "2015-09-24T13:52:49-04:00",
+                                              "description" => "We are glad you are here. Let's get your account set up!",
+                                              "id" => 59,
+                                              "keywords" => nil,
+                                              "legacyStyles" => "1",
+                                              "stateName" => "",
+                                              "title" => "Register",
+                                              "type" => "website",
+                                              "uRL" => "/register"}]}
+
 
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200)
+    with_mocks([ {Pages, [], [get_content_blocks: fn() -> {:ok, 200, @content_block_call} end]},
+                 {Pages, [], [get_system_page: fn("") -> {:ok, 200, @system_page_response} end]},
+                 {Pages, [], [get_site_config: fn(1) -> {:ok, 200, %{}} end]} ]) do
+      conn = get conn, "/"
+      assert html_response(conn, 200)
+    end
   end
 end
 
--- apps/crossroads_interface/test/plugs/page_type_plug_test.exs
+++ apps/crossroads_interface/test/plugs/page_type_plug_test.exs
@@ -1,7 +1,7 @@
 defmodule CrossroadsInterface.Plugs.PageType do
   use CrossroadsInterface.ConnCase
 
-  test "Sets default page type to no_sidebar.html when request to a route is made", %{conn: conn} do
+  test "Sets default page type to no_sidebar.html when request to a route is made", %{conn: _conn} do
     conn = conn() |> CrossroadsInterface.Plug.PageType.call("no_sidebar.html")
     assert conn.assigns.page_type == "no_sidebar.html"
   end
