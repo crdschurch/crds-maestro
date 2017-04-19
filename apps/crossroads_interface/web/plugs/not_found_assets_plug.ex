62477ebaf75121c974e5ab6f8fabd1de30332066
Richie Brees <Brees@MacBook-Pro-4.local>
2017-04-19 09:37:32 -0400

    Removed logging in not_found_assets_plug

--- apps/crossroads_interface/web/plugs/not_found_assets_plug.ex
+++ apps/crossroads_interface/web/plugs/not_found_assets_plug.ex
@@ -1,8 +1,6 @@
 defmodule CrossroadsInterface.Plug.NotFoundAssetsPlug do
   use Plug.Builder
   import Plug.Conn
-  require IEx
-  require Logger
   require Regex
 
   @regex_asset ~r/.*?(\.ico|\.eot|\.svg|\.js|\.css|\.xml|\.less|\.png|\.jpg|\.jpeg|\.gif|\.pdf|\.doc|\.txt|\.ico|\.rss|\.zip|\.mp3|\.rar|\.exe|\.wmv|\.doc|\.avi|\.ppt|\.mpg|\.mpeg|\.tif|\.wav|\.mov|\.psd|\.ai|\.xls|\.mp4|\.m4a|\.swf|\.dat|\.dmg|\.iso|\.flv|\.m4v|\.torrent|\.ttf|\.woff)/
@@ -13,7 +11,6 @@ defmodule CrossroadsInterface.Plug.NotFoundAssetsPlug do
     requested_asset = Enum.at(conn.path_info, -1)
     is_match = Regex.match?(@regex_asset, requested_asset)
     if (is_match) do
-      Logger.debug("sending not found")
       send_resp(conn, 404, "")
       |> halt
     end
