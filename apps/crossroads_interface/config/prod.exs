1b09d9a3dad553a35961f99d2b1b39de1ad6e050
Michael Vezzani <mvezzani@callibrity.com>
2017-04-26 11:18:45 -0400

    US7810: Update README and edit config/prod.exs
    This commit updates the README with information that will help prod
    support to toggle logging on the fly. It also updates production config
    such that when logging files get created, the name of the log file
    reflects in what runway/which environment the logging is happening.

--- apps/crossroads_interface/config/prod.exs
+++ apps/crossroads_interface/config/prod.exs
@@ -1,4 +1,5 @@
 use Mix.Config
+import ConfigHelper, only: [get_suffix: 0]
 
 # For production, we configure the host to read the PORT
 # from the system environment. Therefore, you will need
@@ -26,7 +27,12 @@ config :crossroads_interface, CrossroadsInterface.Endpoint,
   server: true
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger,
+  backends: [{LoggerFileBackend, :debug_log}]
+
+config :logger, :debug_log,
+  path: "/var/log/maestro/maestro#{get_suffix()}.log",
+  level: :debug
 
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
---
+++ apps/crossroads_interface/test/config_helper_test.exs
@@ -0,0 +1,31 @@
+defmodule ConfigHelper.GetSuffixTest do
+  use CrossroadsInterface.ConnCase
+
+  test "get_suffix/0 returns empty string when MAESTRO_NAME_EXTENSION and CRDS_ENV are empty", %{conn: _conn} do
+    System.put_env("MAESTRO_NAME_EXTENSION", "")
+    System.put_env("CRDS_ENV", "")
+
+    assert ConfigHelper.get_suffix() == ""
+  end
+
+  test "get_suffix/0 returns _yahoo when MAESTRO_NAME_EXTENSION is _yahoo and CRDS_ENV is empty", %{conn: _conn} do
+    System.put_env("MAESTRO_NAME_EXTENSION", "_yahoo")
+    System.put_env("CRDS_ENV", "")
+
+    assert ConfigHelper.get_suffix() == "_yahoo"
+  end
+
+  test "get_suffix/0 returns -foobar when MAESTRO_NAME_EXTENSION is empty and CRDS_ENV is foobar", %{conn: _conn} do
+    System.put_env("MAESTRO_NAME_EXTENSION", "")
+    System.put_env("CRDS_ENV", "foobar")
+
+    assert ConfigHelper.get_suffix() == "-foobar"
+  end
+
+  test "get_suffix/0 returns _yahoo-foobar when MAESTRO_NAME_EXTENSION is _yahoo and CRDS_ENV is foobar", %{conn: _conn} do
+    System.put_env("MAESTRO_NAME_EXTENSION", "_yahoo")
+    System.put_env("CRDS_ENV", "foobar")
+
+    assert ConfigHelper.get_suffix() == "_yahoo-foobar"
+  end
+end
