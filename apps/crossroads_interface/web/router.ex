e4c0f2907c53f3bc6549a9c359f9659fc551efbb
Matt Silbernagel <silbermm@gmail.com>
2017-05-04 13:17:57 -0400

    actually I don't need to have to use scopes

--- apps/crossroads_interface/web/router.ex
+++ apps/crossroads_interface/web/router.ex
@@ -19,32 +19,10 @@ defmodule CrossroadsInterface.Router do
     plug :accepts, ["json"]
   end
 
- # scope "/proxy/gateway", CrossroadsInterface do
-    #pipe_through :api
-    #get "/*gateway", ProxyGatewayController, :handle_gateway_proxy
-    #put "/*gateway", ProxyGatewayController, :handle_gateway_proxy
-    #post "/*gateway", ProxyGatewayController, :handle_gateway_proxy
-  #end
-
-  #scope "/proxy/content", CrossroadsInterface do
-    #pipe_through :api
-    #get "/*content", ProxyContentController, :handle_content_proxy
-    #post "/*content", ProxyContentController, :handle_content_proxy
-    #put "/*content", ProxyContentController, :handle_content_proxy
-  #end
-
-  scope "/group-leader", CrossroadsInterface do
-    pipe_through :browser
-    get "/*path", CrdsGroupLeaderController, :index
-  end
-
-  scope "/connect", CrossroadsInterface do
-    pipe_through :browser
-    get "/*path", CrdsConnectController, :index
-  end
-
   scope "/", CrossroadsInterface do
     pipe_through :browser
+    get "/group-leader/*path", CrdsGroupLeaderController, :index
+    get "/connect/*path", CrdsGroupLeaderController, :index
     get "/notfound", NotfoundController, :notfound
     get "/signout", LegacyController, :noRedirect
     get "/signin", LegacyController, :noRedirect
