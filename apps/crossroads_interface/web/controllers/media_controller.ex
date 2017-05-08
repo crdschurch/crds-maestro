f9e8c4f72e795de1b0ceca5d482fcabfe7fb7253
Ken Baum <ken.baum@ingagepartners.com>
2017-05-08 09:30:43 -0400

    US8088 poking and hoping

--- apps/crossroads_interface/web/controllers/media_controller.ex
+++ apps/crossroads_interface/web/controllers/media_controller.ex
@@ -10,16 +10,44 @@ defmodule CrossroadsInterface.MediaController do
   plug :put_layout, "screen_width.html"
 
   def index(conn, _params) do
-    payload = case Pages.get_series_all do
-      {:ok, 200, body} -> Enum.at(body["series"], 0)["title"]
-      {_, _, body} -> body
+    seriesMap = Media.series_all_active()
+
+    [series | _tail] = seriesMap
+
+    _tail
+    |> IO.inspect()
+
+    items = Map.to_list(series)
+    #|> IO.inspect()
+
+    #List.last(items)
+    #|> IO.inspect()
+
+    Enum.each items, fn({_, val})  ->
+        "#{val}"
+        |> IO.inspect()
     end
+
+    #for n <- seriesList, do: seriesList[n]
+
     conn
-    |> render("media.html", %{ payload: payload,
+    |> render("media.html", %{ payload: items,
       "css_files": [
       "/js/legacy/legacy.css"
       ]
     })
   end
+#for n <- [0 .. Enum.count(body["series"])], do: "<p>" <> Enum.at(body["series"],n)["title"] <> "</p>"
+    #length(payload)
+    #|> IO.inspect()
 
+     #payload = Enum.filter(seriesList, fn h -> h != nil end)
+    
+   #payload = Enum.at(seriesList["Series"],0)["title"]
+   # payload = List.count(seriesList)
+    #payload = "Hi there "
+    #Enum.at(seriesList[:series],0)["title"]
+    #payload = seriesList[":series"]["title"]
+        #seriesList
+    #|> IO.inspect()
 end
