7c3d220f22fb41d6ad07d9b696f61cacefc768da
Matt Silbernagel <silbermm@gmail.com>
2017-05-24 10:23:29 -0400

    DE3508 Fix svg in firefox and IE
    * add shim to update svg's with baseUrl

--- apps/crossroads_interface/web/plugs/redirect_cookie_plug.ex
+++ apps/crossroads_interface/web/plugs/redirect_cookie_plug.ex
@@ -1,6 +1,6 @@
 defmodule CrossroadsInterface.Plug.RedirectCookie do
   import Plug.Conn
-
+  require IEx
   def call(conn, default) do
     cookie_domain = Application.get_env(:crossroads_interface, :cookie_domain)
     cookie_options =
@@ -8,7 +8,6 @@ defmodule CrossroadsInterface.Plug.RedirectCookie do
       0 -> [http_only: false]
       _ -> [http_only: false, domain: cookie_domain]
     end
-    conn
-      |> put_resp_cookie("redirectUrl", default, cookie_options)
+    conn |> put_resp_cookie("redirectUrl", default, cookie_options)
   end
 end
