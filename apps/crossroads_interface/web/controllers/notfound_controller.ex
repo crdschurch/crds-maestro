4b6021ab7ad9d6c45fa50dc50c78e9f4cc8bd13f
Matt Silbernagel <silbermm@gmail.com>
2017-06-12 12:52:12 -0400

    don't remove the not found route, but forward on

---
+++ apps/crossroads_interface/web/controllers/notfound_controller.ex
@@ -0,0 +1,19 @@
+defmodule CrossroadsInterface.NotfoundController do
+  use CrossroadsInterface.Web, :controller
+  alias CrossroadsContent.CmsClient
+
+  plug :put_layout, "no_sidebar.html"
+  plug CrossroadsInterface.Plug.Meta
+  plug CrossroadsInterface.Plug.ContentBlocks
+
+  def notfound(conn, _params) do
+    conn
+    |> CrossroadsInterface.Plug.ContentBlocks.call("")
+    |> CrossroadsInterface.Plug.Meta.call("")
+    |> CrossroadsInterface.Plug.PutMetaTemplate.call("meta_tags.html")
+    |> put_layout("no_sidebar.html")
+    |> put_status(404)
+    |> render(CrossroadsInterface.ErrorView, "404.html",
+        %{"css_files": [ "/js/legacy/legacy.css" ]})
+   end
+end
