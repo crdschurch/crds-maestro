729ef48f85dfa3d0af9fe72477ea81d085d7f70b
Charlie Retzler <charlie.retzler@gmail.com>
2017-07-18 15:51:20 -0400

    Moved dependencies and npm install to run at start of 1st container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 set -e
 
-# link from /microclients to crossroads-phoenix/apps/crossroads_interface/priv/static/js
-# TODO: Can we accomplish this a different way? Maybe pointing Phoenix to look at the /microclients folder for it's stuff?
-#rm -rf /crossroads-phoenix/apps/crossroads_interface/priv/static/js && \
-#ln -s /microclients /crossroads-phoenix/apps/crossroads_interface/priv/static/js
+if [ ! -d /crossroads-phoenix/deps ]; then
+    cd /crossroads-phoenix
+    mix deps.get
+fi
 
-# install depdencies
+if [ ! -d /crossroads-phoenix/apps/crossroads_interface/node_modules ]; then
+    cd /crossroads-phoenix/apps/crossroads_interface
+    npm install
+fi
+
 cd /crossroads-phoenix
-mix deps.get
-
 exec "$@"
