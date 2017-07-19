ef7074da51cffc2ea6857d8bab914aaf20183fb4
Samuel Mello <samueljmello@gmail.com>
2017-07-19 16:03:04 -0400

    US9120: Finalize explore route

--- apps/crossroads_interface/web/controllers/explore_controller.ex
+++ apps/crossroads_interface/web/controllers/explore_controller.ex
@@ -11,7 +11,22 @@ defmodule CrossroadsInterface.ExploreController do
   plug CrossroadsInterface.Plug.Meta
 
   def index(conn, _params) do
-    html(conn, File.read!("/js/explore/index.html"))
+    conn
+    |> put_headers
+    |> render_entry("index.html")
+  end
+
+  defp put_headers(conn) do
+    conn |> put_resp_header("content-type", "text/html; charset=utf-8")
+  end
+
+  defp render_entry(conn, file) do
+    path = resolve(file)
+    conn |> Plug.Conn.send_file(200, path)
+  end
+
+  defp resolve(file) do
+    "./priv/static/js/explore/#{file}"
   end
 
 end
