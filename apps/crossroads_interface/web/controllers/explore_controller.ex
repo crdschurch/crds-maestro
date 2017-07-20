59d4737da832cb0aa56cd437e305c2a400025c4f
Samuel Mello <samueljmello@gmail.com>
2017-07-20 11:41:15 -0400

    US9120: condense code via rbarker request

--- apps/crossroads_interface/web/controllers/explore_controller.ex
+++ apps/crossroads_interface/web/controllers/explore_controller.ex
@@ -11,22 +11,7 @@ defmodule CrossroadsInterface.ExploreController do
   plug CrossroadsInterface.Plug.Meta
 
   def index(conn, _params) do
-    conn
-    |> put_headers
-    |> render_entry("index.html")
-  end
-
-  defp put_headers(conn) do
-    conn |> put_resp_header("content-type", "text/html; charset=utf-8")
-  end
-
-  defp render_entry(conn, file) do
-    path = resolve(file)
-    conn |> Plug.Conn.send_file(200, path)
-  end
-
-  defp resolve(file) do
-    "./priv/static/js/static/explore/#{file}"
+    html(conn, File.read!("./priv/static/js/static/explore/index.html"))
   end
 
 end
