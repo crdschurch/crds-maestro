82dd233fed75b9087828b4ab39f16f8e6796715f
Charlie Retzler <charlie.retzler@gmail.com>
2017-08-03 16:15:29 -0400

    Using correct comparison for FORCE env vars

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 set -e
 
-if [ -z "$FORCE_DEPS_GET" ] || [ ! -d /maestro/deps ] || [ ! "$(ls -A /maestro/deps)" ]; then
+if [ ! -z "$FORCE_DEPS_GET" ] || [ ! -d /maestro/deps ] || [ ! "$(ls -A /maestro/deps)" ]; then
     cd /maestro
     mix deps.get
 fi
 
-if [ -z "$FORCE_NPM_INSTALL" ] || [ ! -d /maestro/apps/crossroads_interface/node_modules ] || [ ! "$(ls -A /maestro/apps/crossroads_interface/node_modules)" ] ; then
+if [ ! -z "$FORCE_NPM_INSTALL" ] || [ ! -d /maestro/apps/crossroads_interface/node_modules ] || [ ! "$(ls -A /maestro/apps/crossroads_interface/node_modules)" ] ; then
     cd /maestro/apps/crossroads_interface
     npm install
 fi
 
-if [ -z "$FORCE_MAESTRO_RECOMPILE" ] ; then
+if [ ! -z "$FORCE_MAESTRO_RECOMPILE" ] ; then
     export MIX_PARAMS="$MIX_PARAMS --force"
 fi
 
