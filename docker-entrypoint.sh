1e485bd601ba9ad4ad85dbc6e97667500b07436a
Charlie Retzler <charlie.retzler@gmail.com>
2017-08-04 08:06:38 -0400

    Reworked FORCE_MAESTRO_RECOMPILE option

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+if [ ! -z "$FORCE_MAESTRO_RECOMPILE" ] ; then
+    cd /maestro
+    mix clean --only
+fi
+
 if [ ! -z "$FORCE_DEPS_GET" ] || [ ! -d /maestro/deps ] || [ ! "$(ls -A /maestro/deps)" ]; then
     cd /maestro
     mix deps.get
@@ -9,10 +14,6 @@ fi
 if [ ! -z "$FORCE_NPM_INSTALL" ] || [ ! -d /maestro/apps/crossroads_interface/node_modules ] || [ ! "$(ls -A /maestro/apps/crossroads_interface/node_modules)" ] ; then
     cd /maestro/apps/crossroads_interface
     npm install
-fi
-
-if [ ! -z "$FORCE_MAESTRO_RECOMPILE" ] ; then
-    export MIX_PARAMS="$MIX_PARAMS --force"
 fi
 
 cd /maestro
