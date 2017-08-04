eb1399d51a247f77759cf2b56f0c9e899e603f3a
Charlie Retzler <charlie.retzler@gmail.com>
2017-08-04 08:53:22 -0400

    More hacks to cleanup maestro

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,11 @@ set -e
 
 if [ ! -z "$FORCE_MAESTRO_RECOMPILE" ] ; then
     cd /maestro
+    # This command may fail on windows due to locked file in _build/dev/lib/crossroads_interface/priv/
+    # but it will have made it far enough to force a recompile
+    set +e
     mix clean --only
+    set -e
 fi
 
 if [ ! -z "$FORCE_DEPS_GET" ] || [ ! -d /maestro/deps ] || [ ! "$(ls -A /maestro/deps)" ]; then
