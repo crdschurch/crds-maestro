612eb3aee5e575a225c61b8ab327918b4c0e4201
Samuel Mello <samueljmello@gmail.com>
2017-08-23 16:12:11 -0400

    Another attempt at fixing the problem

--- apps/crossroads_interface/lib/crossroads_interface/endpoint.ex
+++ apps/crossroads_interface/lib/crossroads_interface/endpoint.ex
@@ -1,35 +1,48 @@
 defmodule CrossroadsInterface.Endpoint do
   use Phoenix.Endpoint, otp_app: :crossroads_interface
-  require Logger
 
   socket "/socket", CrossroadsInterface.UserSocket
-
-  application_root_path = Path.join(:code.priv_dir(:crossroads_interface), "static")
-
-  priv_path = ConfigHelper.get_priv_path()
-
-  Logger.debug "Root: #{application_root_path}"
-  Logger.debug "Priv path: #{priv_path}"  
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
 
-  plug Plug.Static, 
-    at: "/", from: application_root_path, gzip: System.get_env("MIX_ENV") == "prod", 
-    only_matching: ["css", "fonts", "assets", "images", "js", "favicon", "robots"] 
+  if System.get_env("MAESTRO_RUN_IN_DOCKER") != nil do
+    
+    plug Plug.Static, 
+      at: "/", from: "/microclients", gzip: System.get_env("MIX_ENV") == "prod", 
+      only_matching: ["css", "fonts", "assets", "images", "js", "favicon", "robots"] 
 
-  plug Plug.Static, 
-    at: "/assets", from: Path.join(priv_path, "js/legacy"), gzip: System.get_env("MIX_ENV") == "prod" 
-   
-  plug Plug.Static, 
-    at: "/explore", from: Path.join(priv_path, "js/static/explore"), gzip: System.get_env("MIX_ENV") == "prod", 
-    cache_control_for_etags: "public, max-age=86400"
+    plug Plug.Static, 
+      at: "/assets", from: "/microclients/js/legacy", gzip: System.get_env("MIX_ENV") == "prod" 
 
-  plug Plug.Static, 
-    at: "/", from: Path.join(priv_path, "js/crds_connect"), gzip: System.get_env("MIX_ENV") == "prod", 
-    only_matching: ["css", "fonts", "assets", "images", "js", "favicon", "robots"]
+    plug Plug.Static, 
+      at: "/explore", from: "/microclients/js/static/explore", gzip: System.get_env("MIX_ENV") == "prod", 
+      cache_control_for_etags: "public, max-age=86400"
+
+    plug Plug.Static, 
+      at: "/", from: "/microclients/js/crds_connect", gzip: System.get_env("MIX_ENV") == "prod", 
+      only_matching: ["css", "fonts", "assets", "images", "js", "favicon", "robots"]
+
+  else
+    
+    plug Plug.Static, 
+      at: "/", from: {:crossroads_interface, "priv/static"}, gzip: System.get_env("MIX_ENV") == "prod", 
+      only_matching: ["css", "fonts", "assets", "images", "js", "favicon", "robots"] 
+
+    plug Plug.Static, 
+      at: "/assets", from: {:crossroads_interface, "priv/static/js/legacy"}, gzip: System.get_env("MIX_ENV") == "prod" 
+    
+    plug Plug.Static, 
+      at: "/explore", from: {:crossroads_interface, "priv/static/js/static/explore"}, gzip: System.get_env("MIX_ENV") == "prod", 
+      cache_control_for_etags: "public, max-age=86400"
+
+    plug Plug.Static, 
+      at: "/", from: {:crossroads_interface, "priv/static/js/crds_connect"}, gzip: System.get_env("MIX_ENV") == "prod", 
+      only_matching: ["css", "fonts", "assets", "images", "js", "favicon", "robots"]
+
+  end
 
   plug CrossroadsInterface.Plug.NotFoundAssetsPlug
 
