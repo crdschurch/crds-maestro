5f60e8ea8ab7d6e5e047ece1ddd39ea57d04954b
Richard Barker <rbarker@Administrators-MacBook-Pro-2.local>
2017-08-24 17:34:13 -0400

    Externalize imgix ImagesLoaded fix

--- apps/crossroads_interface/web/static/js/home_page/card_carousel.js
+++ apps/crossroads_interface/web/static/js/home_page/card_carousel.js
@@ -1,6 +1,9 @@
+import imgixImagesLoaded from '../imgixImagesLoaded';
+
 window.CRDS = window.CRDS || {};
 
-/* global CRDS imagesLoaded Flickity */
+/* global CRDS Flickity */
+
 // ----------------------------------------------- #
 
 CRDS.CardCarousels = function CardCarousels(selector = undefined) {
@@ -40,24 +43,8 @@ CRDS.CardCarousel.prototype.setup = function setup() {
     cards[i].classList.add('carousel-cell');
   }
   const images = this.getImages();
-  this._imagesLoadedImgix(images, () => {
+  imgixImagesLoaded(images, () => {
     window.dispatchEvent(new Event('resize'));
-  });
-};
-
-/* Work around for timing issue in which Flickity ImagesLoaded initializes
-   before Imgix has created src attributes on img tags, therefore it doesn't
-   correctly resize the cards after Imgix is finished.  So detect that and callback */
-CRDS.CardCarousel.prototype._imagesLoadedImgix = function _imagesLoadedImgix(images, callback) {
-  images.forEach((img) => {
-    const attributeCheck = setInterval(() => {
-      if (img.hasAttribute('src')) {
-        clearInterval(attributeCheck);
-        imagesLoaded(img, () => {
-          callback();
-        });
-      }
-    }, 100);
   });
 };
 
