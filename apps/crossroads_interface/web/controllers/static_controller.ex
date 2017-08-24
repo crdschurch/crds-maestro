c3bd8b0a3c4980a2869ea12bb078cae01c3032df
Samuel Mello <samueljmello@gmail.com>
2017-08-24 15:13:13 -0400

    US9121: Removing debug

--- apps/crossroads_interface/web/controllers/static_controller.ex
+++ apps/crossroads_interface/web/controllers/static_controller.ex
@@ -11,9 +11,6 @@ defmodule CrossroadsInterface.StaticController do
 
   def index(conn, _params) do
 
-    Logger.debug "Test:"
-    Logger.debug "#{conn.request_path}"
-
     file = case System.get_env("MAESTRO_RUN_IN_DOCKER") do
       nil -> Path.join(Application.app_dir(:crossroads_interface),"priv/static/js/static#{conn.request_path}/index.html")
       _ -> "/microclients/js/static#{conn.request_path}/index.html"
