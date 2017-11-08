d16899e659a14003861d26a634e39dfc1366b6c1
Rick Barker <rbarker@callibrity.com>
2017-11-08 14:58:00 -0500

    Bit o' cleanup

--- apps/crossroads_interface/test/js/dataTracker_test.js
+++ apps/crossroads_interface/test/js/dataTracker_test.js
@@ -6,60 +6,48 @@ describe('DataTracker', () => {
 
   const testDom = `
     <button data-track-click id="btn-one">Watch Now</button>
-    <button data-track-click="Do The Things" id="btn-two">Do It</button>
-    <form class="searchbar" id="search1" data-track-search>
-      <div class="input-group">
-        <input class="form-control" placeholder="Search by address or zip code" type="text"><span class="input-group-btn"> <button class="btn btn-secondary" type="submit"></button></span>
-      </div>
-    </form>
     <form class="searchbar" id="search2" data-track-search="locationSearch">
       <div class="input-group">
         <input class="form-control" placeholder="Search by address or zip code" type="text"><span class="input-group-btn"> <button class="btn btn-secondary" type="submit"></button></span>
       </div>
-  </form>
+    </form>
   `;
 
   let dataTracker;
-  let btn1;
-  let btn2;
-  let search1;
-  let search2;
+  let btn;
+  let search;
 
   beforeEach(() => {
     document.body.innerHTML = testDom;
     dataTracker = new CRDS.DataTracker();
-    btn1 = document.getElementById('btn-one');
-    btn2 = document.getElementById('btn-two');
-    search1 = document.getElementById('search1');
-    search2 = document.getElementById('search2');
+    btn = document.getElementById('btn-one');
+    search = document.getElementById('search2');
   });
 
   afterEach(() => {
     document.body.innerHTML = '';
     dataTracker = null;
-    btn1 = null;
-    btn2 = null;
-    search1 = null;
-    search2 = null;
+    btn = null;
+    search = null;
   });
 
   it('should send analytics data for search events', () => {
-    search2.getElementsByTagName('input')[0].value = 'foo';
-    search2.getElementsByTagName('button')[0].click();
+    search.getElementsByTagName('input')[0].value = 'foo';
+    search.getElementsByTagName('button')[0].click();
     const trackEventPayload = {
-      Name: search2.dataset.trackSearch || search2.id || 'Unnamed Search',
-      Target: search2.outerHTML,
-      SearchTerm: search2.getElementsByTagName('input')[0].value
+      Name: search.dataset.trackSearch || search.id || 'Unnamed Search',
+      Target: search.outerHTML,
+      SearchTerm: search.getElementsByTagName('input')[0].value
     };
     expect(dataTracker.analytics.track).toHaveBeenCalledWith('Search', trackEventPayload);
   });
 
   it('should send analytics data for click events', () => {
-    btn1.click();
+    btn.click();
     const trackEventPayload = {
-      Name: btn1.dataset.trackClick || btn1.id || 'Unnamed Click Event',
-      Target: btn1.outerHTML,
-      Type: btn1.nodeName
+      Name: btn.dataset.trackClick || btn.id || 'Unnamed Click Event',
+      Target: btn.outerHTML,
+      Type: btn.nodeName
     };
     expect(dataTracker.analytics.track).toHaveBeenCalledWith('ElementClicked', trackEventPayload);
   });
