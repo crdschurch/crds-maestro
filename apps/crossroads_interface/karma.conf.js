da907d32b48544199e85456005287e06d9daf539
Sean C Davis <scdavis41@gmail.com>
2017-12-01 09:58:30 -0500

    US10897 - Abstract Card Carousel Fix broken specs

--- apps/crossroads_interface/karma.conf.js
+++ apps/crossroads_interface/karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
             'node_modules/jquery/dist/jquery.js',
             'node_modules/babel-polyfill/dist/polyfill.js',
             'node_modules/flickity/dist/flickity.pkgd.js',
-            'node_modules/crds-card-carousel/dist/crds-card-carousel-v0.0.1.js',
+            'https://d1tmclqz61gqwd.cloudfront.net/javascripts/crds-card-carousel-v0.1.0.min.js',
             'test/**/*.js'
         ],
 
