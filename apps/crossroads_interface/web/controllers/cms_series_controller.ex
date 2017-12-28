c7ab9fe2a9c2009c256f028a229708cf5d40a7f4
Michael Vezzani <mvezzani@callibrity.com>
2017-12-28 12:00:20 -0500

    DE4788 Minor refactorings to cms_series_controller

--- apps/crossroads_interface/web/controllers/cms_series_controller.ex
+++ apps/crossroads_interface/web/controllers/cms_series_controller.ex
@@ -1,26 +1,22 @@
 defmodule CrossroadsInterface.CmsSeriesController do
   use CrossroadsInterface.Web, :controller
-  alias CrossroadsInterface.Plug
-  require IEx
 
   @base_url Application.get_env(:crossroads_content, :cms_server_endpoint)
   @timeout Application.get_env(:crossroads_content, :cms_timeout)
 
   def show(conn, %{"id" => id}) do
-    {:ok, %{:body => body, :headers => _headers, :request_url => _request_url, :status_code => _status_code }} =
-      "#{@base_url}/api/series?id=#{id}"
-      |> HTTPoison.get(%{"Accept" => "application/json"}, recv_timeout: @timeout)
+    series = get_decoded_series(id)
 
-    decoded_body = Poison.decode!(body)
-    %{"series" => [series]} = decoded_body
     series_still = get_in(series, ["image", "filename"])
     series_title = series["title"]
     series_description = series["description"]
     series_start_date = series["startDate"]
     series_end_date = series["endDate"]
-    series_messages = Enum.map(series["messages"], fn(x) -> %{title: x["title"], message_still: get_in(x, ["messageVideo", "still", "filename"]), message_id: x["id"], message_date: x["date"] } end )
-
-    IEx.pry
+    series_messages = Enum.map(series["messages"], fn(x) ->
+      %{title: x["title"],
+        message_still: get_in(x, ["messageVideo", "still", "filename"]),
+        message_id: x["id"],
+        message_date: x["date"] } end )
 
     render conn, "my_template.html", %{series_title: series_title,
       series_description: series_description,
@@ -28,6 +24,25 @@ defmodule CrossroadsInterface.CmsSeriesController do
       series_start_date: series_start_date,
       series_end_date: series_end_date,
       series_messages: series_messages}
+  end
+
+  defp get_decoded_series(id) do
+    decoded_body = get_body(id)
+      |>Poison.decode!()
+
+    %{"series" => [series]} = decoded_body
+    series
+  end
+
+  defp get_body(id) do
+    {:ok, %{:body => body,
+      :headers => _headers,
+      :request_url => _request_url,
+      :status_code => _status_code }} =
+    "#{@base_url}/api/series?id=#{id}"
+    |> HTTPoison.get(%{"Accept" => "application/json"}, recv_timeout: @timeout)
+
+    body
   end
 end
 
