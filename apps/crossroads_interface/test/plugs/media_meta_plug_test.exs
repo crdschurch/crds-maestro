e2732f3090a90eab13484bb167045e3df073255c
Michael Vezzani <mvezzani@callibrity.com>
2018-01-10 14:03:21 -0500

    DE4867 Add test case for missing image

--- apps/crossroads_interface/test/plugs/media_meta_plug_test.exs
+++ apps/crossroads_interface/test/plugs/media_meta_plug_test.exs
@@ -4,12 +4,17 @@ defmodule CrossroadsInterface.Plugs.MediaMetaTest do
   import Mock
 
   @series_object %{"description" => "Wizard Cow is a hilariously renamed animal as per the internet", 
-                   "image" => %{"filename" => "my_file.jpg"},
-                   "title" => "Wizard Cow",
-                   "className" => "series",
-                   "id" => "222"}
+    "image" => %{"filename" => "my_file.jpg"},
+    "title" => "Wizard Cow",
+    "className" => "series",
+    "id" => "222"}
 
-  test "Sets meta data when request to get a server rendered media page occurs", %{conn: conn} do
+  @series_object_sans_image %{"description" => "Wizard Cow is a hilariously renamed animal as per the internet",
+    "title" => "Wizard Cow",
+    "className" => "series",
+    "id" => "222"}
+
+  test "Sets specific meta data when request to get a server rendered media page occurs", %{conn: conn} do
     with_mock CmsClient, [], [get_site_config: fn(1) -> {:ok, 200, %{}} end] do
       app_env = Application.get_env(:crossroads_interface, :cookie_domain)
       conn = CrossroadsInterface.Plug.MediaMeta.call(conn, @series_object)
@@ -20,6 +25,19 @@ defmodule CrossroadsInterface.Plugs.MediaMetaTest do
       assert conn.assigns.meta_type == "website"
       assert conn.assigns.meta_image == "my_file.jpg"
     end
-  end       
+  end
+
+  test "Sets default meta image when no image is present", %{conn: conn} do
+    with_mock CmsClient, [], [get_site_config: fn(1) -> {:ok, 200, %{}} end] do
+      app_env = Application.get_env(:crossroads_interface, :cookie_domain)
+      conn = CrossroadsInterface.Plug.MediaMeta.call(conn, @series_object_sans_image)
+  
+      assert conn.assigns.meta_title == "Wizard Cow | Crossroads"
+      assert conn.assigns.meta_description == "Wizard Cow is a hilariously renamed animal as per the internet"
+      assert conn.assigns.meta_url == "https://#{if app_env != "", do: app_env, else: "www"}.crossroads.net/series/222/wizard-cow"
+      assert conn.assigns.meta_type == "website"
+      assert conn.assigns.meta_image == "http://crds-cms-uploads.imgix.net/content/images/cr-social-sharing-still-bg.jpg"
+    end
+  end
 end
   
