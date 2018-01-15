159a9f75a7986c01fbb449fa81753234500b8029
Michael Vezzani <mvezzani@callibrity.com>
2018-01-15 13:48:08 -0500

    Add styles and refactor controller

--- apps/crossroads_interface/web/controllers/articles_controller.ex
+++ apps/crossroads_interface/web/controllers/articles_controller.ex
@@ -1,15 +1,22 @@
 defmodule CrossroadsInterface.ArticlesController do
   use CrossroadsInterface.Web, :controller
-  require IEx
+  alias CrossroadsInterface.Plug
+
+  plug Plug.Meta
+  plug Plug.ContentBlocks
+  plug Plug.BodyClass, "crds-styles"
+
 
   def index(conn, _) do
     {:ok, resp} = HTTPoison.get "https://medium.com/crdschurch/?format=json"
     posts = String.replace_leading(resp.body, "])}while(1);</x>", "")
             |> Poison.decode!
             |> get_in(["payload", "references", "Post"])
+    posts = prepare_posts(posts)
 
-    IEx.pry
-    render(conn, "index.html", %{posts: posts,
+    conn
+    |> put_layout("screen_width.html")
+    |> render("index.html", %{posts: posts,
       css_files: [ "/css/app.css", "/js/legacy/legacy.css" ]})
   end
 
@@ -18,8 +25,22 @@ defmodule CrossroadsInterface.ArticlesController do
     body = String.replace_leading(resp.body, "])}while(1);</x>", "")
            |> Poison.decode!
     paragraphs = get_in(body, ["payload", "value", "content", "bodyModel", "paragraphs"])
-    render(conn, "show.html", %{paragraphs: paragraphs,
-      css_files: [ "/css/app.css", "/js/legacy/legacy.css" ]})
 
+    conn
+    |> put_layout("centered_content_page.html")
+    |> render("show.html", %{paragraphs: paragraphs,
+      css_files: [ "/css/app.css", "/js/legacy/legacy.css" ]})
+  end
+
+  defp prepare_posts(posts) do
+    Enum.map(posts, fn(post) ->
+      {id, body} = post
+
+      %{title: body["title"], id: body["id"], slug: body["uniqueSlug"],
+        subtitle: body["content"]["subtitle"],
+        word_count: body["virtuals"]["wordCount"],
+        reading_time: body["virtuals"]["readingTime"],
+        image: body["virtuals"]["previewImage"]["imageId"]}
+    end)
   end
 end
--- apps/crossroads_interface/web/views/articles_view.ex
+++ apps/crossroads_interface/web/views/articles_view.ex
@@ -1,28 +1,3 @@
 defmodule CrossroadsInterface.ArticlesView do
   use CrossroadsInterface.Web, :view
-
-  def get_title_from_post(post) do
-    {id, %{"title" => title}} = post
-    title
-  end
-
-  def create_link(post) do
-    {id, %{"title" => title}} = post
-    String.downcase(title)
-    |> remove_punctuation
-    |> linkify_string
-    |> add_id(id)
-  end
-
-  def remove_punctuation(string) do
-    String.replace(string, ~r/[\p{P}\p{S}]/, "")
-  end
-
-  def linkify_string(string) do
-    String.replace(string, " ", "-")
-  end
-
-  def add_id(title, id) do
-    title <> "-" <> id
-  end
 end
