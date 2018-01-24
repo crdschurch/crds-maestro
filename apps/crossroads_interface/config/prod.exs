d8cdf4c8ab8eb0b3d44b9190985da3dbcb401197
Samuel Mello <samueljmello@gmail.com>
2018-01-24 09:38:08 -0500

    US11263: FIX

--- apps/crossroads_interface/config/prod.exs
+++ apps/crossroads_interface/config/prod.exs
@@ -2,17 +2,17 @@ use Mix.Config
 
 if "#{System.get_env("MAESTRO_SSL")}" == "true" do
   config :crossroads_interface, CrossroadsInterface.Endpoint,
-    https: [port: {:system, "MAESTRO_PORT"},
+    https: [port: System.get_env("MAESTRO_PORT"),
       otp_app: :crossroads_interface,
       keyfile: "/certificates/tls.key",
       certfile: "/certificates/tls.crt"]
 else
   config :crossroads_interface, CrossroadsInterface.Endpoint,
-    http: [port: {:system, "MAESTRO_PORT"}, otp_app: :crossroads_interface]
+    http: [port: System.get_env("MAESTRO_PORT"), otp_app: :crossroads_interface]
 end
 
 config :crossroads_interface, CrossroadsInterface.Endpoint,
-  url: [host: {:system, "MAESTRO_HOSTNAME"}, port: {:system, "MAESTRO_PORT"}],
+  url: [host: System.get_env("MAESTRO_HOSTNAME"), port: System.get_env("MAESTRO_PORT")],
   cache_static_manifest: "priv/static/manifest.json",
   server: true
 
