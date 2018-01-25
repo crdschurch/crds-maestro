27e1f3b31fe4ad4218c7a02685bd8be047f52392
Sean C Davis <scdavis41@gmail.com>
2018-01-25 16:11:20 -0500

    US10236 - Live Smooth Scrolling Replace slim web component with a jQuery script for smooth scrolling

--- apps/crossroads_interface/web/static/js/web_components/smooth_scroller.js
+++ apps/crossroads_interface/web/static/js/web_components/smooth_scroller.js
@@ -1,27 +1,9 @@
-// window.__forceSmoothScrollPolyfill__ = true;
 require('smoothscroll-polyfill').polyfill();
 
-if (typeof Slim === 'undefined') {
-  require('slim-js');
-}
-
-Slim.tag(
-  'smooth-scroller',
-  `<span bind click="scroll">{{label}}</span>`,
-  class SmoothScroller extends Slim {
-
-    onAdded() {
-      this.label = this.getAttribute('label');
-      $(document).ready(() => {
-        this.scrollTargetId = this.getAttribute('scroll-to');
-        this.scrollTarget = document.getElementById(this.scrollTargetId);
-      });
-    }
-
-    scroll() {
-      this.scrollTarget.scrollIntoView({
-        behavior: 'smooth'
-      });
-    }
-  }
-);
+$(document).click('[data-smooth-scroll-to]', function(event) {
+  event.preventDefault();
+  var targetId = $(event.target).data('smooth-scroll-to');
+  document.getElementById(targetId).scrollIntoView({
+    behavior: 'smooth'
+  });
+});
