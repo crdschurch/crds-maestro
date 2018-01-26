8f506642774a9aada9fca939fb607ce3aeca7797
Sean C Davis <scdavis41@gmail.com>
2018-01-26 11:29:15 -0500

    US10236 - Live Smooth Scrolling Update smooth scroller to allow clicking on children

--- apps/crossroads_interface/web/static/js/web_components/smooth_scroller.js
+++ apps/crossroads_interface/web/static/js/web_components/smooth_scroller.js
@@ -1,9 +1,13 @@
 require('smoothscroll-polyfill').polyfill();
 
-$(document).click('[data-smooth-scroll-to]', function(event) {
-  event.preventDefault();
-  var targetId = $(event.target).data('smooth-scroll-to');
-  document.getElementById(targetId).scrollIntoView({
-    behavior: 'smooth'
+$(document).ready(function(event) {
+  $('[data-smooth-scroll-to]').click(function(event) {
+    event.preventDefault();
+    var targetId = $(this).data('smooth-scroll-to');
+    var target = document.getElementById(targetId)
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+    return true;
   });
 });
