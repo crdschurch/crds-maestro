3cb882d478c8410938342841e288515a797a06c1
Michael Vezzani <mvezzani@callibrity.com>
2018-02-19 09:49:40 -0500

    US12193 Add articles index view
    This commit enables making calls to the microservice to fetch articles
    and render them into the articles template at /articles.

--- apps/crossroads_interface/web/controllers/articles_controller.ex
+++ apps/crossroads_interface/web/controllers/articles_controller.ex
@@ -5,42 +5,19 @@ defmodule CrossroadsInterface.ArticlesController do
   plug Plug.Meta
   plug Plug.ContentBlocks
   plug Plug.BodyClass, "crds-styles"
+  require IEx
 
 
   def index(conn, _) do
-    {:ok, resp} = HTTPoison.get "https://medium.com/crdschurch/?format=json"
-    posts = String.replace_leading(resp.body, "])}while(1);</x>", "")
-            |> Poison.decode!
-            |> get_in(["payload", "references", "Post"])
-    posts = prepare_posts(posts)
+    {:ok, resp} = HTTPoison.get "http://localhost:5000/api/content/articles"
+    %{body: posts, headers: _headers, request_url: _req, status_code: _stat} = resp
+
+    IEx.pry
+    posts = Poison.decode!(posts)
 
     conn
     |> put_layout("screen_width.html")
-    |> render("index.html", %{posts: posts,
+    |> render("index.html", %{ posts: posts,
       css_files: [ "/css/app.css", "/js/legacy/legacy.css" ]})
-  end
-
-  def show(conn, %{"id" => id}) do
-    {:ok, resp} = HTTPoison.get "https://medium.com/crdschurch/#{id}?format=json"
-    body = String.replace_leading(resp.body, "])}while(1);</x>", "")
-           |> Poison.decode!
-    paragraphs = get_in(body, ["payload", "value", "content", "bodyModel", "paragraphs"])
-
-    conn
-    |> put_layout("centered_content_page.html")
-    |> render("show.html", %{paragraphs: paragraphs,
-      css_files: [ "/css/app.css", "/js/legacy/legacy.css" ]})
-  end
-
-  defp prepare_posts(posts) do
-    Enum.map(posts, fn(post) ->
-      {id, body} = post
-
-      %{title: body["title"], id: body["id"], slug: body["uniqueSlug"],
-        subtitle: body["content"]["subtitle"],
-        word_count: body["virtuals"]["wordCount"],
-        reading_time: body["virtuals"]["readingTime"],
-        image: body["virtuals"]["previewImage"]["imageId"]}
-    end)
   end
 end
