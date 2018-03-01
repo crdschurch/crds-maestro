e7e5046488b10a21952a669b0092a392624a8698
Michael Vezzani <mvezzani@callibrity.com>
2018-03-01 11:33:48 -0500

    US12193 Add sensible fallbacks if env variable is not present

--- apps/crossroads_content/config/dev.exs
+++ apps/crossroads_content/config/dev.exs
@@ -2,4 +2,4 @@ use Mix.Config
 
 config :crossroads_content,
   cms_server_endpoint: System.get_env("CRDS_CMS_SERVER_ENDPOINT") || "https://contentint.crossroads.net/",
-  publications_server_endpoint: System.get_env("CRDS_PUBLICATIONS_ENDPOINT")
+  publications_server_endpoint: System.get_env("CRDS_PUBLICATIONS_ENDPOINT") || "https://gatewayint.crossroads.net/content/api/"
