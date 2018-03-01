736a741f94f76052da3ce6c4cc1ae233390e4ac2
Luke Moderwell <luke.moderwell@gmail.com>
2018-03-01 16:49:25 -0500

    merging

--- apps/crossroads_interface/web/views/publications_view.ex
+++ apps/crossroads_interface/web/views/publications_view.ex
@@ -9,4 +9,22 @@ defmodule CrossroadsInterface.PublicationsView do
     get_in(content, ["heroImg", "caption"])
   end
 
+  def get_most_recent(articles) do
+    reversed_articles = Enum.reverse(articles)
+    Enum.take(reversed_articles, 1)
+  end
+
+  def get_featured(articles) do
+    reversed_articles = Enum.reverse(articles)
+    Enum.slice(reversed_articles, 2..7)
+  end
+
+  def get_unfeatured(articles) do
+    reversed_articles = Enum.reverse(articles)
+    split = Enum.split(reversed_articles, 8)
+    |> Tuple.to_list()
+    [head | tail] = split
+    [head2 | tail2] = tail
+    head2
+  end
 end
