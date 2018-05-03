20fc552130bad606e0e269283ad461ae3561c503
Dillon Courts <dillon.courts@gmail.com>
2018-05-03 12:30:20 -0400

    Adding a step to confirm deployment is complete before flush

--- deployment/kubernetes/flush-varnish-cache.sh
+++ deployment/kubernetes/flush-varnish-cache.sh
@@ -6,10 +6,11 @@ echo "======== Pod flush on K8S =======";
 echo "=================================";
 echo "";
 
-DEPLOYMENT=$1
+DEPLOYMENT_TO_FLUSH=$1
+DEPLOYMENT_TO_CHECK=$2
 FAILED=0
 
-if [[ -z ${DEPLOYMENT+x} || ${DEPLOYMENT} = "" ]]; then
+if [[ -z ${DEPLOYMENT_TO_FLUSH+x} || ${DEPLOYMENT_TO_FLUSH} = "" ]]; then
     FAILED=1
     echo "ERROR: You must provide the name of the Varnish deployment"
 fi
@@ -19,8 +20,20 @@ if [[ ${FAILED} = 1 ]]; then
     exit 1;
 fi
 
+if [[ -n ${DEPLOYMENT_TO_CHECK} && ${DEPLOYMENT_TO_CHECK} != "" ]]; then
+    NUM_REPLICAS_EXPECTED="$(kubectl get deployments --selector=app=${DEPLOYMENT_TO_CHECK} -ao jsonpath='{.items[0].spec.replicas}')"
+    NUM_REPLICAS_CURRENT="0"
+    echo "Checking replica count"
+    echo "Current Replicas: ${NUM_REPLICAS_CURRENT}/${NUM_REPLICAS_EXPECTED}"
+    while [ ${NUM_REPLICAS_EXPECTED} != ${NUM_REPLICAS_CURRENT} ]; do
+        echo "Current Replicas: ${NUM_REPLICAS_CURRENT}/${NUM_REPLICAS_EXPECTED}"
+        NUM_REPLICAS_CURRENT="$(kubectl get deployments --selector=app=${DEPLOYMENT_TO_CHECK} -ao jsonpath='{.items[0].status.replicas}')"
+        sleep 3s
+    done
+fi
+
 echo -n " - Getting pods... "
-PODS="$(kubectl get pods --selector=app=${DEPLOYMENT} -ao jsonpath='{range .items[*]}{@.metadata.name}{" "}{@.spec.template.spec.containers[].image}{"\n"}{end}')"
+PODS="$(kubectl get pods --selector=app=${DEPLOYMENT_TO_FLUSH} -ao jsonpath='{range .items[*]}{@.metadata.name}{" "}{@.spec.template.spec.containers[].image}{"\n"}{end}')"
 
 if [[ $? -ne 0 && ${#PODS[@]} -eq 0 ]]; then
     echo "failed."
