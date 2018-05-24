c962b1c4be4e2bfc971827f91da2ab201b2ac9a7
Dillon Courts <dillon.courts@gmail.com>
2018-05-24 19:30:31 -0400

    Rolling back the nginx as background process for maestro

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i "s/{{CRDS_PRERENDER_IO_KEY}}/${CRDS_PRERENDER_IO_KEY}/" /etc/nginx/conf.d/default.conf
+# No longer running nginx in background
+#sed -i "s/{{CRDS_PRERENDER_IO_KEY}}/${CRDS_PRERENDER_IO_KEY}/" /etc/nginx/conf.d/default.conf
 
 echo "Maestro for production started." \
-  && nginx \
   && /maestro/bin/crossroads_interface foreground
